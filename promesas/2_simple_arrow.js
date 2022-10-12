e386e57dfbf09b0a0f42bc8213fe07f779783635
Usuario <Email Usuario>
2022-10-12 15:59:27 -0500

    jhbkhb

--- promesas/2_simple_arrow.js
+++ promesas/2_simple_arrow.js
@@ -3,24 +3,24 @@ const xmlHttpRequest = require('xmlhttprequest').XMLHttpRequest
 const url = "https://pokeapi.co/api/v2/type"
 //funcion para conectar a una api en modo asincrono
 const get_data = (endpoint, exito, fallo)=>{
-    //Definir la promesa
-    let promise = new Promise((resolve, rejected)=>{
-        
-    })
-    //1. Crear objeto xmlhttp
-    const h = new xmlHttpRequest()
-    //2. Abrir una conexion a la API
-    h.open('GET', endpoint)
-    //3. Enviar la request a serve(API)
-    h.send()
-    //4. Tratamiento de datos de la response
-    h.onload = ()=>{
+        //Definir la promesa
+        let promise = new Promise((resolve, rejected)=>{
+        //1. Crear objeto xmlhttp
+        const h = new xmlHttpRequest()
+        //2. Abrir una conexion a la API
+        h.open('GET', endpoint)
+        //3. Enviar la request a serve(API)
+        h.send()
+        //4. Tratamiento de datos de la response
+        h.onload = ()=>{
         if(h.status === 200){
             exito(h.responseText)
         }else{
             fallo(h.status)
         }
     }
+    })
+    
 }
 //funcion callback: exito
 const exito = (response)=>{
